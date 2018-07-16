96bd914ff9c774a47c27e707495373d4342a0d42
X1 Carbon <yjl9903@vip.qq.com>
2018-07-16 14:04:12 +0800

    1028 DP

--- 1028.cpp
+++ 1028.cpp
@@ -6,9 +6,10 @@ using namespace std;
 typedef long long ll;
 const int maxn = 120 + 5;
 
-int c1[maxn], c2[maxn], n;
+int c1[maxn], c2[maxn], n, dp[maxn];
 
 int main(){
+    // 母函数
     c1[0] = 1;
     for (int i = 1; i < maxn; i++){
         for (int j = 0; j < maxn; j += i){
@@ -20,8 +21,16 @@ int main(){
             c1[j] = c2[j]; c2[j] = 0;
         }
     }
+    // DP: dp[i]表示组成i的方案数
+    dp[0] = 1;
+    for (int i = 1; i < maxn; i++){
+        for (int j = 0; i + j < maxn; j++){
+            dp[j + i] += dp[j];
+        }
+    }
     while (cin >> n){
-        cout << c1[n] << endl;
+        // cout << c1[n] << endl;
+        cout << dp[n] << endl;
     }
     return 0;
 }
